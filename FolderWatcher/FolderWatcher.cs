61f5d584bc71c646e5cfc74a25dcb44e595aca72
skiedeisch <scott.kiedeisch@ascensus.com>
2018-06-02 13:13:13 -0400

    current state of project

---
+++ FolderWatcher/FolderWatcher.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace FolderWatcher
+{
+    class FolderWatcher
+    {
+        private readonly FileSystemWatcher FileSystemWatcher;
+
+        public FolderWatcher(string path)
+        {
+            // TODO bad path handling
+            FileSystemWatcher = new FileSystemWatcher(path);
+            Initialize();
+        }
+
+        public FolderWatcher(string path, string filterString)
+        {
+            // TODO bad path and filter string handling
+            FileSystemWatcher = new FileSystemWatcher(path, filterString);
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            FileSystemWatcher.Created += OnCreated;
+            FileSystemWatcher.Deleted += OnDeleted;
+            FileSystemWatcher.Changed += OnChanged;
+            FileSystemWatcher.Renamed += OnRenamed;
+            FileSystemWatcher.Error += OnError;
+            //FileSystemWatcher.IncludeSubdirectories = true;
+            FileSystemWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+        }
+
+        public bool Start()
+        {
+            if (IsWatching())
+            {
+                return false;
+            }
+
+            Output($"Starting watcher for {FileSystemWatcher.Path} with filter string {FileSystemWatcher.Filter}");
+            FileSystemWatcher.EnableRaisingEvents = true;
+            return true;
+        }
+        
+        public bool Stop()
+        {
+            if (!IsWatching())
+            {
+                return false;
+            }
+
+            FileSystemWatcher.EnableRaisingEvents = false;
+            Output("Stopped watcher for " + FileSystemWatcher.Path);
+            return true;
+        }
+
+        public bool IsWatching()
+        {
+            return FileSystemWatcher.EnableRaisingEvents;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Output(string.Format("Create {0}", e.Name));
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Output(string.Format("Delete {0}", e.Name));
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Output(string.Format("Change {0}", e.Name));
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Output(string.Format("Rename {0} -> {1}", e.OldName, e.Name));
+        }
+
+        private void OnError(object sender, ErrorEventArgs e)
+        {
+            Output(string.Format("Exception {0}", e.GetException()));
+        }
+
+        private void Output(string msg)
+        {
+            Console.WriteLine(DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture) + ": " + msg);
+        }
+    }
+}
---
+++ FolderWatcher/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+//using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace FolderWatcher
+{
+    class Program
+    {
+        private string path;
+        private string filterstring;
+
+        static void Main(string[] args)
+        {
+            //ILoggerFactory logger = new LoggerFactory();
+            //ILogger l = logger.CreateLogger("asdf");
+            //l.LogInformation("hello");
+
+            Log.Logger = new LoggerConfiguration().WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+            Log.Information("hello, can you hear me?");
+
+            try
+            {
+
+                IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                string path = configuration["Path"];
+                string filterString = configuration["FilterString"];
+
+                //string path = System.Configuration.ConfigurationManager.AppSettings[""];
+                FolderWatcher watcher = new FolderWatcher(path, filterString);
+                watcher.Start();
+                Console.ReadLine();
+                watcher.Stop();
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                // TODO log
+                Console.WriteLine("Unexpected exception occurred. See error log for details.");
+                Console.ReadLine();
+            }
+        }
+        
+
+        private void ParseArguments(string[] args)
+        {
+            if (args.Length == 0 || args.Length > 2)
+            {
+                Console.WriteLine("Requires path argument");
+            }
+
+            if (args.Length >= 1)
+            {
+                path = args[0];
+            }
+            if (args.Length >= 2)
+            {
+                filterstring = args[1];
+            }
+        }
+    }
+}
