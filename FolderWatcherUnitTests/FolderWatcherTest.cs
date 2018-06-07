e5457c31e4e62acb5b52995fe7144ec99c39b7e1
skiedeisch <scott.kiedeisch@ascensus.com>
2018-06-07 19:15:35 -0400

    completed basic unit tests

--- FolderWatcherUnitTests/FolderWatcherTest.cs
+++ FolderWatcherUnitTests/FolderWatcherTest.cs
@@ -9,27 +9,15 @@ namespace FolderWatcherUnitTests
     public class FolderWatcherTest
     {
         [Fact]
-        public void ConstructorExceptionIfPassedNull()
-        {
-            Assert.Throws<ArgumentException>("fileSystemWatcher", () => new FolderWatcher.FolderWatcher(null));
-        }
-
-        [Fact]
-        public void ConstructorIsSuccessful()
+        public void ConstructorDoesNotThrowExceptions()
         {
             new FolderWatcher.FolderWatcher(A.Dummy<IFileSystemWatcher>());
         }
 
         [Fact]
-        public void DoesNotStartIfAlreadyStarted()
+        public void ConstructorThrowsExceptionIfPassedNull()
         {
-            IFileSystemWatcher fakeFileSystemWatcher = A.Fake<IFileSystemWatcher>();
-            A.CallTo(() => fakeFileSystemWatcher.EnableRaisingEvents).Returns(true);
-            FolderWatcher.FolderWatcher folderWatcher = new FolderWatcher.FolderWatcher(fakeFileSystemWatcher);
-
-            bool isStarted = folderWatcher.Start();
-
-            Assert.False(isStarted);
+            Assert.Throws<ArgumentException>("fileSystemWatcher", () => new FolderWatcher.FolderWatcher(null));
         }
 
         [Fact]
@@ -43,6 +31,67 @@ namespace FolderWatcherUnitTests
 
             Assert.True(isStarted);
             A.CallToSet(() => fakeFileSystemWatcher.EnableRaisingEvents).To(true).MustHaveHappened();
+        }
+
+        [Fact]
+        public void StartReturnsFalseIfAlreadyStarted()
+        {
+            IFileSystemWatcher fakeFileSystemWatcher = A.Fake<IFileSystemWatcher>();
+            A.CallTo(() => fakeFileSystemWatcher.EnableRaisingEvents).Returns(true);
+            FolderWatcher.FolderWatcher folderWatcher = new FolderWatcher.FolderWatcher(fakeFileSystemWatcher);
+
+            bool isStarted = folderWatcher.Start();
+
+            Assert.False(isStarted);
+        }
+
+        [Fact]
+        public void Stops()
+        {
+            IFileSystemWatcher fakeFileSystemWatcher = A.Fake<IFileSystemWatcher>();
+            A.CallTo(() => fakeFileSystemWatcher.EnableRaisingEvents).Returns(true);
+            FolderWatcher.FolderWatcher folderWatcher = new FolderWatcher.FolderWatcher(fakeFileSystemWatcher);
+
+            bool isStopped = folderWatcher.Stop();
+
+            Assert.True(isStopped);
+            A.CallToSet(() => fakeFileSystemWatcher.EnableRaisingEvents).To(false).MustHaveHappened();
+        }
+
+        [Fact]
+        public void StopReturnsFalseIfAlreadyStopped()
+        {
+            IFileSystemWatcher fakeFileSystemWatcher = A.Fake<IFileSystemWatcher>();
+            A.CallTo(() => fakeFileSystemWatcher.EnableRaisingEvents).Returns(false);
+            FolderWatcher.FolderWatcher folderWatcher = new FolderWatcher.FolderWatcher(fakeFileSystemWatcher);
+
+            bool isStopped = folderWatcher.Stop();
+
+            Assert.False(isStopped);
+        }
+
+        [Fact]
+        public void IsWatchingWhenEnableRaisingEventsIsTrue()
+        {
+            IFileSystemWatcher fakeFileSystemWatcher = A.Fake<IFileSystemWatcher>();
+            A.CallTo(() => fakeFileSystemWatcher.EnableRaisingEvents).Returns(true);
+            FolderWatcher.FolderWatcher folderWatcher = new FolderWatcher.FolderWatcher(fakeFileSystemWatcher);
+
+            bool isWatching = folderWatcher.IsWatching();
+
+            Assert.True(isWatching);
+        }
+
+        [Fact]
+        public void IsNotWatchingWhenEnableRaisingEventsIsFalse()
+        {
+            IFileSystemWatcher fakeFileSystemWatcher = A.Fake<IFileSystemWatcher>();
+            A.CallTo(() => fakeFileSystemWatcher.EnableRaisingEvents).Returns(false);
+            FolderWatcher.FolderWatcher folderWatcher = new FolderWatcher.FolderWatcher(fakeFileSystemWatcher);
+
+            bool isWatching = folderWatcher.IsWatching();
+
+            Assert.False(isWatching);
         }
     }
 }
